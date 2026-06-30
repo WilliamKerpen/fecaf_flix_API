9e33f7666d3cb8aa1f28e5a6885550af4a9abf9d
WilliamKerpen <williamkerpen@hotmail.com>
2026-06-30 21:13:36 +0200

    correcao da rota Delet user

--- src/routes/routersUser.js
+++ src/routes/routersUser.js
@@ -8,6 +8,6 @@ const routerUser = express.Router();
 routerUser.post('/user', cadastrarUser); // Cadastro de usuário normal
 routerUser.post('/login', loginUser); // Login de usuário normal
 routerUser.put('/user/:id_user', autenticarToken, putUser); // Atualizar usuário normal (precisa estar logado)
-router.delete('/user', autenticarToken, deletarUserController); // deletar Usuario
+routerUser.delete('/user', autenticarToken, deletarUserController); // deletar Usuario
 
 export default routerUser;
